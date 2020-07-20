c49d94ca109e0fd4ffc4b4a6fabc705807e503dc
Viacheslav Sapronov <iamsvv@gmail.com>
2020-07-20 14:54:13 +0300

    add domains process

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -5,15 +5,27 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class DomainController extends Controller
 {
     public function add(Request $request)
     {
-        $domain = $request->get('domain');
-        DB::table('domains')->insert(
-            ['name' => $domain]
-        );
-        return $domain['name'];
+        $queryDomain = $request->get('domain');
+        $parsedDomain = parse_url($queryDomain['name']);
+        $domain = mb_strtolower("{$parsedDomain['scheme']}://{$parsedDomain['host']}");
+        $hasDomain = DB::table('domains')
+            ->where('name', $domain)
+            ->exists();
+        if (!$hasDomain) {
+            DB::table('domains')->insert(
+                [
+                    'name' => $domain,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ]
+            );
+        }
+        return var_dump($domain);
     }
 }
